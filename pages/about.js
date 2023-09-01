c7da2c814b9f841a79396407589d4f9aeffd992c
karthiabega <karthiabega@gmail.com>
2023-09-01 12:57:05 +0530

    Added about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import styles from '../styles/About.module.css';
+
+
+const About = () => {
+    return (
+      <h1 className={styles.title}>aboutus</h1>
+    );
+  };
+  
+  export default About;
